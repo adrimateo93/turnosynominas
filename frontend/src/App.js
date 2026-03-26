1ee6d59660a0c4f69996bb7d886dd0beb7072dfe
adrimateo93 <270978693+adrimateo93@users.noreply.github.com>
2026-03-27 00:25:05 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,8 @@ import Register from "@/pages/Register";
 import Dashboard from "@/pages/Dashboard";
 import Settings from "@/pages/Settings";
 import Payroll from "@/pages/Payroll";
-
-const BACKEND_URL = "https://seguriturno.onrender.com";
-export const API = `${BACKEND_URL}/api`; 
+const BACKEND_URL = "https://seguriturno1-5.onrender.com";
+export const API = `${BACKEND_URL}/api`;
 
 // Register Service Worker for PWA and Push Notifications
 if ('serviceWorker' in navigator) {
